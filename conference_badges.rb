a3610be205eb94f8415aa6f2c45736e3c435f603
Maverick Adolfo <merkonical@gmail.com>
2019-08-17 07:39:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,6 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |badge|
     puts badge
   end
-  assign_rooms(attendees).each do |attendee|
-    puts attendee
-  end 
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
 end 
