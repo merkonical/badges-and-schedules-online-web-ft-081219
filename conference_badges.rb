13580f2aa58f01410c155c0a4bcfeff9cfd92c8f
Maverick Adolfo <merkonical@gmail.com>
2019-08-17 06:58:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,8 @@
 # Write your code here.
 
-def badge_maker(name)
-  puts "Hello, my name is #{name}"
+  def badge_maker(name)
+    puts "Hello, my name is #{name}"
+  end
+  return badge_maker
 end 
-return badge_maker
+
