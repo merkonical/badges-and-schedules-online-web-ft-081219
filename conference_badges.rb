692ee2a6079f832cc85285f75240cf6e0850919f
Maverick Adolfo <merkonical@gmail.com>
2019-08-17 07:33:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,4 +11,7 @@ def batch_badge_creator(attendees)
 end 
 
 def assign_rooms(attendees)
-  attendees.map do |attendee, index|
+  attendees.each_with_index.map do |attendee, index|
+    "Hello, #{attendee}! You'll be assigned to room #{index}!"
+  end
+end 
